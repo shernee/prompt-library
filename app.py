ef5b745b9c2853a2fdd75002de54125b74c92909
Neetha Sherra <shernee93@gmail.com>
2023-09-27 18:11:19 -0700

    adding prompts

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import pinecone
 import os
+import openai
 
 app = Flask(__name__)
+
+def generate_embedding(prompt):
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    prompt_vector = openai.Embedding.create(
+        input=prompt,
+        model="text-embedding-ada-002"
+    )["data"][0]["embedding"]
+    return prompt_vector
 
 @app.route("/")
 def hello_world():
     return render_template('home.html')
 
-@app.route("/search_results", methods=["POST"])
+@app.route("/search-results", methods=["POST"])
 def search_results():
     user_prompt = request.form.get('user_prompt')
     print(user_prompt)
     if user_prompt:
         pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="gcp-starter")
-        pinecone.create_index("prompt-library", dimension=8, metric="euclidean")
-        pinecone.describe_index("prompt-library")
-        # index = pinecone.Index(index_name="prompt_library")
+        # pinecone.delete_index("prompt-library")
+        # pinecone.create_index("prompt-library", dimension=1536, metric="euclidean")
+        index = pinecone.Index(index_name="prompt_library")
 
         # response_vector = index.query(queries=[user_prompt], top_k=1)
 
@@ -29,10 +38,22 @@ def search_results():
         return jsonify({"response": indexes})
     else:
         return jsonify({"response": "Please provide a user prompt."})
+    
+@app.route("/add-prompt", methods=["POST"])
+def add_prompt():
+    prompt = request.form.get('prompt')
+    if prompt:
+        embedding = generate_embedding(prompt)
 
-@app.route("/test_api", methods=["POST"])
-def test_api():
-    user_prompt = request.form.get('user_prompt')
-    print(user_prompt)
+        pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="gcp-starter")
+        index = pinecone.Index(index_name="prompt-library")
+        pinecone_vector = (prompt, embedding)
+        upsert_count = index.upsert(vectors=[pinecone_vector])['upserted_count']
+        index_stats = index.describe_index_stats()
+        response = f"Your prompt has been added! Total prompt count {index_stats['total_vector_count']}"
+        if upsert_count:
+            return jsonify({"response": response})
+    else:
+        return jsonify({"response": "Please provide a user prompt."})
+    
 
-    pinecone.init(api_key=os.environ["PINECONE_API_KEY"])
